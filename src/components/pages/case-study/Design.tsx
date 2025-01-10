5a1341aca04ad24477a1191a0b43bf62c6361f1a
jessekerch <jesse.kercheval@gmail.com>
2025-01-10 14:08:13 -0800

    fix: start design content structure
    Co-authored-by: Shane Ziegler <shanedb@gmail.com>
    Co-authored-by: joelbarton-io <joelbarton406@gmail.com>
    Co-authored-by: Wook Kim <h.wook.kim@gmail.com>

--- src/components/pages/case-study/Design.tsx
+++ src/components/pages/case-study/Design.tsx
@@ -1,174 +1,64 @@
+import { useContext } from "react";
+import { PageNavigationContext } from "@/providers/PageNavigation";
+import { ImageContentModal } from "@/components/ui/dialog";
+
+import { useInView } from "react-intersection-observer";
+
+// Importing images
+import CiCdCircles from "@/assets/2.1.1.ci-cd-simple-circles.png";
+
+const SectionInView = ({
+  sectionId,
+  onInView,
+}: {
+  sectionId: string;
+  onInView: (id: string) => void;
+}) => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.3,
+    rootMargin: "0px 0px -60% 0px",
+  });
+
+  if (inView) onInView(sectionId);
+
+  return <div ref={ref}></div>;
+};
+
 const Design = () => {
+  const pageContext = useContext(PageNavigationContext);
+
+  if (!pageContext) {
+    throw new Error(
+      "make sure to wrap the component in a PageNavigationProvider",
+    );
+  }
+
+  const { setActiveSubheader, pages, activePage } = pageContext;
+
+  const subheaderIds = pages[activePage].subheaders?.map(
+    (subheader) => subheader.id,
+  );
+
+  const subheaderNames = pages[activePage].subheaders?.map(
+    (subheader) => subheader.name,
+  );
+
+  const handleInView = (id: string) => {
+    const subheaderIndex = subheaderIds.indexOf(id);
+    if (subheaderIndex !== -1) {
+      setActiveSubheader(subheaderIndex);
+    }
+  };
+
   return (
     <>
-      <article id="design-1">
-        <h2>Design 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-2">
-        <h2>Design 2</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-3">
-        <h2>Design 3</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-4">
-        <h2>Design 4</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-5">
-        <h2>Design 5</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-6">
-        <h2>Design 6</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-7">
-        <h2>Design 7</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
-      <article id="design-8">
-        <h2>Design 8</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit
-          sequi delectus ipsum nihil nostrum magnam saepe cum debitis! Eaque
-          facere nulla facilis hic, modi dolorem temporibus vel quas tempora?
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
-          tempora repudiandae? Quasi error sed voluptatem, quisquam ullam at
-          accusantium? Aliquid iure modi beatae, ex delectus voluptas eius.
-          Maxime, harum eveniet.
-        </p>
-      </article>
+      <section id="design-0">
+        <SectionInView sectionId="design-0" onInView={handleInView} />
+        <h2>{subheaderNames[0]}</h2>
+        <p></p>
+        <ImageContentModal src={CiCdCircles} alt={"CI/CD process"} />
+      </section>
     </>
   );
 };
