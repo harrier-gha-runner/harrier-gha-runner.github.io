546410044959755db5be778189ad43a044ba7a61
jessekerch <jesse.kercheval@gmail.com>
2025-01-10 14:08:35 -0800

    fix: add section titles to page navigation context
    Co-authored-by: Shane Ziegler <shanedb@gmail.com>
    Co-authored-by: joelbarton-io <joelbarton406@gmail.com>
    Co-authored-by: Wook Kim <h.wook.kim@gmail.com>

--- src/providers/PageNavigation.tsx
+++ src/providers/PageNavigation.tsx
@@ -29,23 +29,71 @@ const PageNavigationProvider = ({ children }: { children: ReactNode }) => {
       id: "problem-domain",
       name: "Problem Domain",
       subheaders: [
-        { id: "problem-domain-1", name: "Problem Domain 1" },
-        { id: "problem-domain-2", name: "Problem Domain 2" },
-        { id: "problem-domain-3", name: "Problem Domain 3" },
+        { id: "problem-domain-2-0", name: "2. Problem Domain" },
+        {
+          id: "problem-domain-2-1",
+          name: "2.1 GitHub Actions: A Powerful CI/CD Tool",
+        },
+        {
+          id: "problem-domain-2-1-1",
+          name: "2.1.1 CI/CD and DevOps: The Backbone of Modern Software Development",
+        },
+        {
+          id: "problem-domain-2-1-2",
+          name: "2.1.2 GHA Workflow Automation",
+        },
+        {
+          id: "problem-domain-2-1-3",
+          name: "2.1.3 GHA and CI/CD automation",
+        },
+        {
+          id: "problem-domain-2-2",
+          name: "2.2 Slower than desirable GHA Automation",
+        },
+        {
+          id: "problem-domain-2-2-1",
+          name: "2.2.1 Unpacking GHA’s original design",
+        },
+        {
+          id: "problem-domain-2-2-2",
+          name: "2.2.2 Limitations of GHA Cache Action",
+        },
+        {
+          id: "problem-domain-2-3",
+          name: "2.3 Accelerate GHA Workflows with Cache",
+        },
+        {
+          id: "problem-domain-2-3-1",
+          name: "2.3.1 Implementation Options for self-hosted runners",
+        },
+        {
+          id: "problem-domain-2-3-2",
+          name: "2.3.2 Opportunity for 3rd-Party-Supported DIY",
+        },
       ],
     },
     {
       id: "design",
       name: "Design",
       subheaders: [
-        { id: "design-1", name: "Design 1" },
-        { id: "design-2", name: "Design 2" },
-        { id: "design-3", name: "Design 3" },
-        { id: "design-4", name: "Design 4" },
-        { id: "design-5", name: "Design 5" },
-        { id: "design-6", name: "Design 6" },
-        { id: "design-7", name: "Design 7" },
-        { id: "design-8", name: "Design 8" },
+        { id: "design-1", name: "3. Harrier Design" },
+        {
+          id: "design-1",
+          name: "3.1 Isolated Runner Environment in User’s Cloud",
+        },
+        { id: "design-2", name: "3.2 Warm-pool of self-hosted runners" },
+        {
+          id: "design-3",
+          name: "3.3 Connect Runners with Specific Workflow Jobs",
+        },
+        { id: "design-4", name: "3.4 Termination of ephemeral runner" },
+        { id: "design-5", name: "3.5 Dedicated Persistent Cache Store" },
+        {
+          id: "design-6",
+          name: "3.6 Out-of-the-box cache management of dependencies",
+        },
+        { id: "design-7", name: "3.7 Workflow-driven start/stop of runners" },
+        { id: "design-8", name: "3.8 Minimal workflow modification" },
       ],
     },
     {
@@ -53,21 +101,29 @@ const PageNavigationProvider = ({ children }: { children: ReactNode }) => {
       name: "Implementation",
       subheaders: [
         {
+          id: "implementation-0",
+          name: "4. Harrier Implementation",
+        },
+        {
           id: "implementation-1",
-          name: "4.1 Isolated VPC in User’s AWS Account",
+          name: "4.1 Isolated VPC in User's AWS Account",
         },
         {
           id: "implementation-2",
           name: "4.2 Fleet of EC2 runners placed in standby",
         },
-        { id: "implementation-3", name: "Implementation 3" },
-        { id: "implementation-4", name: "Implementation 4" },
-        { id: "implementation-5", name: "Implementation 5" },
-        { id: "implementation-6", name: "Implementation 6" },
-        { id: "implementation-7", name: "Implementation 7" },
-        { id: "implementation-8", name: "Implementation 8" },
-        { id: "implementation-9", name: "Implementation 9" },
-        { id: "implementation-10", name: "Implementation 10" },
+        {
+          id: "implementation-3",
+          name: "4.3 Just-In-Time Token Registration of Runner",
+        },
+        { id: "implementation-4", name: "4.4 Termination of EC2 runners" },
+        { id: "implementation-5", name: "4.5 S3 Bucket Cache Store" },
+        { id: "implementation-6", name: "4.6 Node modules cache" },
+        { id: "implementation-7", name: "4.7 API Platform Integration" },
+        { id: "implementation-8", name: "4.8 Minimal Workflow Modification" },
+        { id: "implementation-9", name: "4.9 Overall Architecture" },
+        { id: "implementation-10", name: "4.10 Deployment" },
+        { id: "implementation-11", name: "4.11 Performance benchmarks" },
       ],
     },
     {
