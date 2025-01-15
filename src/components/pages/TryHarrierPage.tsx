64863d2da01a8255706072955d20bb538ecff114
joelbarton-io <joelbarton406@gmail.com>
2025-01-14 20:31:34 -0700

    chore: begin adding tryHarrierPage content and added initial screenshots/userjourney
    Co-authored-by: Jesse Kercheval <jessekerch@users.noreply.github.com>
    Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>
    Co-authored-by: Wook Kim <wook2000@users.noreply.github.com>

--- src/components/pages/TryHarrierPage.tsx
+++ src/components/pages/TryHarrierPage.tsx
@@ -1,17 +1,27 @@
-import SetupForm from "@/components/utility/SetupForm";
-import RenderYaml from "@/components/utility/RenderYaml";
-import { useState } from "react";
-
 const TryHarrierPage = () => {
-  const [formDataJSON, setFormDataJSON] = useState("");
   return (
     <>
       <h2 className="text-center text-3xl font-bold">Get Started</h2>
-      {formDataJSON === "" ? (
-        <SetupForm setFormDataJSON={setFormDataJSON} />
-      ) : (
-        <RenderYaml formDataJSON={formDataJSON} />
-      )}
+      <section id="try-harrier-1">1</section>
+      <section id="try-harrier-2">2</section>
+      <section id="try-harrier-3">3</section>
+      <section id="try-harrier-4">4</section>
+      {/* 
+        sections:
+        1. Setup AWS OIDC role => grab OIDC role arn(?) to provide to Harrier, With appropriate permissions
+        2. Get Github Organization PAT
+           Generate PAT w/ correct scoping and permissions => place PAT into AWS secrets manager
+        3. Choose self-hosted runner infra config
+            Bare minimum of description for each config variable necessary
+        4. Click generate YAML and download
+            Upload to repo and run workflow
+
+
+        desired ui features:
+         1. screenshot carousel for each step (where necessary)
+         2. form validation for each user setting options dropdown
+         3. download/copy yaml code button
+      */}
     </>
   );
 };
